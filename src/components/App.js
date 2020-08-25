d0753c7060145115e42f0b148f21c1eef83adaa8
JP <jpucket8@gmail.com>
2020-08-25 06:26:26 -0500

    switched to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter,
+  HashRouter,
   Route,
   Switch
 } from 'react-router-dom';
@@ -15,12 +15,12 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <div className="container">
       <Header />
       
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/treehouse" component={Home} />
         <Route path="/about" render={ () => <About title='About' /> } />
         <Route exact path="/teachers" component={Teachers} />
         <Route path="/teachers/:topic/:name" component={Featured} />
@@ -28,7 +28,7 @@ const App = () => (
         <Route component={NotFound} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
