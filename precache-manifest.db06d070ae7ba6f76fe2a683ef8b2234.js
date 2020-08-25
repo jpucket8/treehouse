539e3d5d57ed325dceac375892f91e0050aac7b3
JP <jpucket8@gmail.com>
2020-08-25 06:28:45 -0500

    Updates

---
+++ precache-manifest.db06d070ae7ba6f76fe2a683ef8b2234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aab9477fa5663876773",
+    "url": "/treehouse/static/css/main.5c552bd4.chunk.css"
+  },
+  {
+    "revision": "5aab9477fa5663876773",
+    "url": "/treehouse/static/js/main.5aab9477.chunk.js"
+  },
+  {
+    "revision": "493f53f4e1f39a9d0cc3",
+    "url": "/treehouse/static/js/1.493f53f4.chunk.js"
+  },
+  {
+    "revision": "69b45a228753f9cdb428",
+    "url": "/treehouse/static/js/runtime~main.69b45a22.js"
+  },
+  {
+    "revision": "05a725144fb8bd0e5c1bd2e44a6b46d1",
+    "url": "/treehouse/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/treehouse/precache-manifest.cda5c2e11bdfd7c3c761bc932ba9968f.js"
+  "/treehouse/precache-manifest.db06d070ae7ba6f76fe2a683ef8b2234.js"
 );
 
 workbox.clientsClaim();
